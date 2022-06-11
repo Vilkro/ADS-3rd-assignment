3ce36b4beaf42d0ecbb53ab5b48b2bc6b7e0c8a9
Vilkro <96818891+Vilkro@users.noreply.github.com>
2022-06-11 15:30:53 +0600

    Add files via upload

---
+++ pa3/src/com/company/Main (task 3).java
@@ -0,0 +1,25 @@
+package com.company;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if(args.length < 1) {
+            System.out.println("Error, usage: java ClassName inputfile");
+            System.exit(1);
+        }
+        Scanner reader = new Scanner(new FileInputStream(args[0]));
+
+        Set<String> student = new HashSet<>();
+        while (reader.hasNext()){
+            String lhs = reader.next();
+            String rhs = reader.next();
+            int chs = reader.nextInt();
+            if(chs > 1) {
+                student.add(lhs + " " + rhs + " " + chs);
+            }
+        }
+        student.stream().sorted().forEach(e -> System.out.println(e));
+    }
+}
